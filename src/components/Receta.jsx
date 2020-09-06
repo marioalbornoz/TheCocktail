4f3851ce77a88e36e605f93814c27c0144d875e6
marioalbornoz <mario.albornozc@utem.cl>
2020-09-06 13:11:30 -0300

    bootstrap a cards

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 export const Receta = ({receta}) => {
     return (
-      <div className="col-md-4">
+      <div className="col-md-4 mb-4">
         <div className="card">
           <h2 className="card-header">{receta.strDrink}</h2>
+          <img className="card-img-top" src={receta.strDrinkThumb} alt=""/>
         </div>
       </div>
     );
