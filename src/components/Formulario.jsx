9ebf87a4bb6eee756d65be4598500f68fcd718fe
marioalbornoz <mario.albornozc@utem.cl>
2020-09-05 20:32:23 -0400

    Se guardan los datos seleccionados en el form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { CategoriasContext } from '../context/CategoriasContext';
 
 export const Formulario = () => {
 
+    const [ busqueda, guardarBusqueda] = useState({
+        nombre:"",
+        categoria:""
+    })
+
     const { categorias } = useContext(CategoriasContext);
-    console.log(categorias);    
+    console.log(categorias); 
+    
+    // funcion para leer el contenido
+    const obtenerDatosReceta = (e) => {
+        guardarBusqueda({
+          ...busqueda,
+          [e.target.name]: e.target.value,
+        });
+    }
 
     return (
       <form className="col-12">
@@ -18,17 +31,34 @@ export const Formulario = () => {
               className="form-control"
               type="text"
               placeholder="Buscar por ingrediente"
-              autoComplete="off"
+              onChange={obtenerDatosReceta}
             />
           </div>
           <div className="col-md-4">
-            <select name="categoria" className="form-control" id="">
-                <option value="">-Selecciona una opcion</option>
+            <select
+              name="categoria"
+              onChange={obtenerDatosReceta}
+              className="form-control"
+              id=""
+            >
+              <option value="">-Selecciona una opcion</option>
+              {categorias.map((categoria) => (
+                <option
+                  key={categoria.strCategory}
+                  value={categoria.strCategory}
+                >
+                  {categoria.strCategory}
+                </option>
+              ))}
             </select>
           </div>
           <div className="col-md-4">
-                    <input type="submit" className="btn btn-block btn-primary" value="Buscar recetas"/>
-                </div>
+            <input
+              type="submit"
+              className="btn btn-block btn-primary"
+              value="Buscar recetas"
+            />
+          </div>
         </div>
       </form>
     );
