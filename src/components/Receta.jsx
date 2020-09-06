e8fd9bcbd676487b4679cd753dc4cc48a7aeaf60
marioalbornoz <mario.albornozc@utem.cl>
2020-09-06 13:17:18 -0300

    mas clases de bootstrap agregadas

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -5,7 +5,13 @@ export const Receta = ({receta}) => {
       <div className="col-md-4 mb-4">
         <div className="card">
           <h2 className="card-header">{receta.strDrink}</h2>
-          <img className="card-img-top" src={receta.strDrinkThumb} alt=""/>
+          <img className="card-img-top" src={receta.strDrinkThumb} alt={`Imagen de ${receta.srtDrink}`}/>
+          <div className="card-body">
+          <button
+            className="btn btn-block btn-primary"
+            type="button"
+            >Ver receta</button>
+          </div>
         </div>
       </div>
     );
